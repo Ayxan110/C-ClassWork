6dc5472312184b11bc233e9967598e31d51123af
Aykhan Z. Dadashov <AykhanZd@CODE.LOCAL>
2019-11-06 18:17:20 +0400

    added

--- ConsoleApp1/Algorithms/Algorithm.cs
+++ ConsoleApp1/Algorithms/Algorithm.cs
@@ -28,14 +28,137 @@
         {
             int index = 0;
             int wrtlength = GetLength(wrt) - GetLength(phrase);
-            for (int i = 0; i < wrtlength; i++)
+            for (int i = 0; i <= wrtlength; i++)
             {
                 if (wrt.Substring(i, GetLength(phrase)) == phrase)
                 {
                     index = i;
+                    break;
                 }
             }
             return index;
+        }
+        public string Substring(int index, int count, string wrt)
+        {
+            string phrase = string.Empty;
+            for (int i = index; i < index + count; i++)
+            {
+                phrase += wrt[i];
+            }
+            return phrase;
+        }
+        public string Replace(string oldPhrase, string newPhrase, string wrt)
+        {
+            int beginPoint = IndexOf(oldPhrase, wrt);
+            int endPointofOldValue = beginPoint + oldPhrase.Length;
+            string leftSide = Substring(0, beginPoint, wrt);
+            string rightSide = Substring(endPointofOldValue,GetLength(wrt)-endPointofOldValue,wrt);
+            return $"{leftSide}{newPhrase}{rightSide}";
+        }
+        public string Insert(int index, string insertword, string wrt)
+        {
+            string leftside = Substring(0, index, wrt);
+            string rightside = Substring(index, GetLength(wrt) - GetLength(leftside), wrt);
+            return $"{leftside}{insertword}{rightside}";
+        }
+        public string Remove(int index, string wrt)
+        {
+            string leftside = Substring(0, index, wrt);
+            return $"{leftside}";
+        }
+        public string RemoveBetween(int index, int count, string wrt)
+        {
+            string remain = Substring(0, index, wrt);
+            string remain2 = Substring(index + count, GetLength(wrt) - (index + count), wrt);
+            return $"{remain}{remain2}";
+        }
+        public string PadLeft(int index, char symbol, string wrt)
+        {
+            int global = index - GetLength(wrt);
+            string result = Generator(global, symbol);
+            return $"{result}{wrt}";
+        }
+        public string PadRight(int index, char symbol, string wrt)
+        {
+            int global = index - GetLength(wrt);
+            string result = Generator(global, symbol);
+            return $"{wrt}{result}";
+        }
+        public string Generator(int total, char symbol)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < total; i++)
+            {
+                result += symbol;
+            }
+            return result;
+        }
+        public bool StartsWith(string phrase, string wrt)
+        {
+            bool response = false;
+            if (Substring(0, GetLength(phrase), wrt) == phrase)
+            {
+                response = true;
+            }
+            return response;
+        }
+        public bool EndsWith(string phrase, string wrt)
+        {
+            bool response = false;
+            string leftside = Substring(0, GetLength(wrt) - GetLength(phrase), wrt);
+            if (Substring(GetLength(leftside), GetLength(wrt) - GetLength(leftside), wrt) == phrase)
+            {
+                response = true;
+            }
+            return response;
+        }
+        public int LastIndexOf(string phrase, string wrt)
+        {
+            int index = 0;
+            int wrtlength = GetLength(wrt) - GetLength(phrase);
+            for (int i = wrtlength; i > 0; i--)
+            {
+                if (wrt.Substring(i, GetLength(phrase)) == phrase)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+        public string TrimStart(char symbol, string wrt)
+        {
+            int count = 0;
+            for (int i = 0; i < GetLength(wrt); i++)
+            {
+                if (wrt[i] == symbol)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string newwrt = RemoveBetween(0, count, wrt);
+            return newwrt;
+        }
+        public string TrimEnd(char symbol, string wrt)
+        {
+            int count = 0;
+            for (int i = GetLength(wrt) - 1; i >= 0; i--)
+            {
+                if (wrt[i] == symbol)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string newwrt = Substring(0, GetLength(wrt) - count, wrt);
+            return newwrt;
         }
     }
 }
--- ConsoleApp1/Algorithms/Program.cs
+++ ConsoleApp1/Algorithms/Program.cs
@@ -6,12 +6,24 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            string str = "Aykhan Dadashov";
+            string str = "///Aykhan Dadashov///";
             Algorithm algo = new Algorithm();
 
-            WriteLine(algo.GetLength(str));
-            WriteLine(algo.Contains("hgjan", str));
-            WriteLine(algo.IndexOf("D", str));
+            //WriteLine(algo.GetLength(str));
+            //WriteLine(algo.Contains("an", str));
+            //WriteLine(algo.IndexOf("ov", str));
+            //WriteLine(algo.Substring(14, 1, str));
+            WriteLine(algo.Replace("Aykhan", "Ayxan", str));
+            //WriteLine(algo.Insert(4, "a", str));
+            //WriteLine(algo.Remove(4, str));
+            //WriteLine(algo.RemoveBetween(0, 4, str));
+            //WriteLine(algo.PadLeft(23, 'h', str));
+            //WriteLine(algo.PadRight(23, 'h', str));
+            //WriteLine(algo.StartsWith("Ay", str));
+            //WriteLine(algo.EndsWith("an", str));
+            //WriteLine(algo.LastIndexOf("ov", str));
+            WriteLine(algo.TrimStart('/', str));
+            WriteLine(algo.TrimEnd('/', str));
             ReadKey();
         }
     }
