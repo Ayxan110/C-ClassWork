4f66f722e81316241a977753fc160f341ad087b7
Ayxan110 <aykhanzd@code.edu.az>
2019-11-19 05:58:42 -0800

    added

---
+++ LogViewer/LogViewer/Database.cs
@@ -0,0 +1,8 @@
+﻿namespace LogViewer
+{
+    abstract class Database
+    {
+        public abstract void AddText(string text);
+        public abstract string[] Read();
+    }
+}
---
+++ LogViewer/LogViewer/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogViewer
+{
+    public partial class Form1 : Form
+    {
+        Database db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new FileReader();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            db.AddText(txbx_write.Text);
+            txbx_write.Clear();
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            foreach(var item in db.Read())
+            {
+                listBox.Items.Add(item);
+            }
+        }
+    }
+}
